ca7eb635e2c06ec9bb8899ffc533de686aa49656
J0natha <jonathamenezes@outlook.com>
2023-12-01 10:34:56 -0300

    Comentários modificados organização da classe e dos imports

--- LoRa_send/RandomDelay.cpp
+++ LoRa_send/RandomDelay.cpp
@@ -1,12 +1,12 @@
 #include <sys/types.h>
-#include "RandomDelay.hpp"
 #include <Arduino.h>
 
-/// Returns a random delay in milisseconds given the lower (inclusive) and upper (exclusive) limits
-/// lowerLimit has the default value of 0, while upperLimit has 1000 as default
+#include "RandomDelay.hpp"
+
+/// Retorna um long sem sinal pseudo-aleatório dados os limites inferior (incluindo) e superior (excluindo),
+/// os argumentos são opcionais e têm valor padrão de 0 e 1000 respectivamente
 u_long RandomDelay::randomDelay(u_long lowerLimit=0, u_long upperLimit=1000)
 {
   u_long range = upperLimit - lowerLimit;
   return (rand() % range) + lowerLimit;
 }
-
--- LoRa_send/RandomDelay.hpp
+++ LoRa_send/RandomDelay.hpp
@@ -4,13 +4,9 @@
 
 class RandomDelay
 {
-private:
-
-  RandomDelay() { }
-  static void swap(u_long & value1, u_long & value2);
-
 public:
-
   static u_long randomDelay(u_long lowerLimit, u_long upperLimit);
 
+private:
+  RandomDelay() { }
 };
